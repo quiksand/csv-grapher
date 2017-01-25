cc13655673d11ada968fec898fa4c010cae75878
Cody Schindler <admin@codyschindler.com>
2017-01-25 00:05:04 -0800

    cleanup for readability, and new features!

--- csvcomp.py
+++ csvcomp.py
@@ -1,95 +1,93 @@
 import csv
-# import sys
 import argparse
 import os
 import matplotlib
 matplotlib.use('TkAgg')
-# from matplotlib.widgets import MultiCursor
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mtick
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.backends.backend_tkagg import NavigationToolbar2TkAgg
 import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog
 from tkinter import messagebox
-# from tkinter.filedialog import askopenfilename
-# from tkinter.messagebox import showerror
-
-#graphs dict holds all series to be plotted - Move inside Series class
-graphs = {}
 
 #Series object stores information about the series to be graphed
 class Series:
-    def __init__(self, graph_path):
+    obj_list = {}
+    def __init__(self, path_to_csv):
         self.show = True
         self.x = []
         self.y = []
-        self.graph_path = graph_path
-        self.label = os.path.split(graph_path)[1].lower()[:-4]
-        with open(graph_path, newline='') as csvfile:
+        self.csv_path = path_to_csv
+        self.label = os.path.split(self.csv_path)[1].lower()[:-4]
+        self.readin_csv()
+        # self.remove_title_rows()
+        Series.obj_list[self.label] = self
+    def readin_csv(self):
+        with open(self.csv_path, newline='') as csvfile:
             reader = csv.reader(csvfile)
             for row in reader:
                 self.x.append(row[0])
                 self.y.append(row[1])
-        graphs[self.label] = self
+    def remove_title_rows(self):
+        print("TODO: REMOVE TITLE ROWS")
+        # make some code to remove title rows
     def __del__(self):
-        del graphs[self.label]
+        del Series.obj_list[self.label]
     def __str__(self):
-        return self.graph_path
+        return self.csv_path
     def get_attr(self):
-        print(self.label, self.graph_path, self.show)
+        print(self.label, self.csv_path, self.show)
     def show(self):
         self.show = True
     def hide(self):
         self.show = False
+    def plot_series():
+        self.fig = plt.figure()
+        p1 = fig.add_subplot(111)
+        # p1 = fig.add_subplot(211)
+        # p2 = fig.add_subplot(223)
+        # p3 = fig.add_subplot(224)
+        for series in Series.obj_list.values():
+            p1.plot(series.x, series.y, label=series.label)
+            # p2.plot(Series.obj_list[series].x, Series.obj_list[series].y, label=Series.obj_list[series].label)
+            # p3.plot(Series.obj_list[series].x, Series.obj_list[series].y, label=Series.obj_list[series].label)
+        #adjust settings
+        p1.set_xlabel('Frequency (kHz)')
+        p1.set_ylabel('Amplitude (V)')
+        p1.yaxis.set_major_formatter(mtick.FormatStrFormatter('%.0e'))
+        p1.set_xlim(0, 1)
+        plt.legend()
+        return fig
 
 #checks if csv file exists in specified path
-def check_path(graph_path):
-    return os.path.isfile(graph_path) and graph_path.lower().endswith(".csv")
+def check_path(csv_path):
+    return os.path.isfile(csv_path) and csv_path.lower().endswith(".csv")
 
 def export_csv():
-    print('TODO: Export CSV')
-    write_csv('../CombinedCSV.csv')
-
+    fname = tk.filedialog.asksaveasfilename(filetypes=(("csv", "*.csv"),
+        ("All files", "*.*") ))
+    if fname:
+        write_csv(fname)
 
 #TODO: Needs a lot of work
 def plot_graphs():
-    # set up figure
-    fig = plt.figure()#figsize=(9,5))
+    fig = plt.figure()
     p1 = fig.add_subplot(111)
     # p1 = fig.add_subplot(211)
     # p2 = fig.add_subplot(223)
     # p3 = fig.add_subplot(224)
-
-    for series in graphs:
-        # print(graphs[series].get_attr())
-        p1.plot(graphs[series].x, graphs[series].y, label=graphs[series].label)
-        # p2.plot(graphs[series].x, graphs[series].y, label=graphs[series].label)
-        # p3.plot(graphs[series].x, graphs[series].y, label=graphs[series].label)
-
+    for series in Series.obj_list.values():
+        p1.plot(series.x, series.y, label=series.label)
+        # p2.plot(Series.obj_list[series].x, Series.obj_list[series].y, label=Series.obj_list[series].label)
+        # p3.plot(Series.obj_list[series].x, Series.obj_list[series].y, label=Series.obj_list[series].label)
     #adjust settings
-    # multi = MultiCursor(fig.canvas, (p1, p2), color='r', lw=1,
-                    # horizOn=False, vertOn=True)
     p1.set_xlabel('Frequency (kHz)')
     p1.set_ylabel('Amplitude (V)')
-    p1.set_xlim(0, 1)
     p1.yaxis.set_major_formatter(mtick.FormatStrFormatter('%.0e'))
-    # p2.set_xlabel('Frequency (kHz)')
-    # p2.set_ylabel('Amplitude (V)')
-    # p2.set_ylim(0, 0.00015)
-    # p2.set_xlim(2, 12)
-    # p2.yaxis.set_major_formatter(mtick.FormatStrFormatter('%.00e'))
-    # p3.set_xlabel('Frequency (kHz)')
-    # p3.set_ylabel('Amplitude (V)')
-    # p3.set_ylim(0, 0.0001)
-    # p3.set_xlim(36, 40)
-    # p3.yaxis.set_major_formatter(mtick.FormatStrFormatter('%.0e'))
-    # p3.autoscale_view()
-    # plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    p1.set_xlim(0, 1)
     plt.legend()
-    # plt.show()
-    # fig.tight_layout()
-    # plt.subplots_adjust(right=0.8)
     return fig
 
 #TODO: Needs a lot of work
@@ -98,13 +96,13 @@ def write_csv(of):
     xs = []
     ys = []
     rows = []
-    for series in graphs:
-        labels.append(graphs[series].label)
-        xs.append(graphs[series].x)
-        ys.append(graphs[series].y)
+    for series in Series.obj_list.values():
+        if series.show:
+            labels.append(series.label)
+            xs.append(series.x)
+            ys.append(series.y)
     rows.append(labels)
     for index, val in enumerate(xs[0], start = 0):
-        # print(index, val)
         rows.append([])
         rows[index+1].append(xs[0][index])
         for y in ys:
@@ -114,21 +112,16 @@ def write_csv(of):
         writer.writerows(rows)
 
 def load_file():
-    fname = tk.filedialog.askopenfilename(filetypes=(("Template files", "*.tplate"),
-                                       ("HTML files", "*.html;*.htm"),
-                                       ("All files", "*.*") ))
+    fname = tk.filedialog.askopenfilename(filetypes=(("Csv files", "*.csv"),
+        ("All files", "*.*")))
     if fname:
         try:
-            print("""here it comes: self.settings["template"].set(fname)""")
+            Series(fname)
         except:                     # <- naked except is a bad idea
             tk.messagebox.showerror("Open Source File", "Failed to read file\n'%s'" % fname)
         return
 
-
-
-
 def main():
-
     #argument parsing
     parser = argparse.ArgumentParser(description='Plots one or more .csv files')
     parser.add_argument('-f', dest='f', required=False,
@@ -139,57 +132,42 @@ def main():
         help='Specify an output filename to condense all csv files into one')
     args = parser.parse_args()
 
-
-    root = tk.Tk()
-    root.title("CSV Grapher")
-    # content = ttk.Frame(root)
-    # content.grid()
-    # frame = ttk.Frame(content, borderwidth=5, relief="sunken", width=200, height=100)
-    # frame.grid()
-    # namelbl = ttk.Label(content, text="Name")
-    # name = ttk.Entry(content)
-
-
     #procedure if file is specified
     if args.f:
-        #open file and save to graphs dict if correct format
-        graph_path = os.path.abspath(args.f)
-        if not check_path(graph_path):
+        csv_path = os.path.abspath(args.f)
+        if not check_path(csv_path):
             print("Error: File path does not exist or is not correct format")
             return 1
-        # create_graph(graph_path)
-        Series(graph_path)
+        # create_graph(csv_path)
+        Series(csv_path)
 
     #procedure if directory is given or file not specified
     else:
         #open files in directory and save to graphs dict if correct format
         for f in os.listdir(args.dir):
-            graph_path = os.path.abspath(os.path.join(args.dir, f))
-            if check_path(graph_path):
-                Series(graph_path)
+            csv_path = os.path.abspath(os.path.join(args.dir, f))
+            if check_path(csv_path):
+                Series(csv_path)
+
+    root = tk.Tk()
+    root.title("CSV Grapher")
 
     #plot that ish
     fig = plot_graphs()
 
     # a tk.DrawingArea
     canvas = FigureCanvasTkAgg(fig, master=root)
-    # canvas.show()
-    # canvas.get_tk_widget().grid()
     canvas.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
 
     toolbar = NavigationToolbar2TkAgg(canvas, root)
     toolbar.update()
-    # canvas._tkcanvas.pack()
     canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
-
 
     def _quit():
         root.quit()     # stops mainloop
         root.destroy()  # this is necessary on Windows to prevent
                         # Fatal Python Error: PyEval_RestoreThread: NULL tstate
 
-    # newframe = tk.Frame(master=root)
-    # newframe.grid(column=10, row=10)
     export_button = ttk.Button(master=root, text='Export CSV', command=export_csv)
     export_button.pack()
 
@@ -203,61 +181,7 @@ def main():
     # If you put root.destroy() here, it will cause an error if
     # the window is closed with the window manager.
 
-
-
-
-
-
-
-    # plt.show()
-
-    #remove when feature is better developed
-    #for now, combine everything in graphs into one csv and print it
-    # write_csv(args.of)
-
-    #it's over... go home.
-    print("Terminating script")
-
-
-    # app.mainloop()
-
     return 0
 
 if __name__ == '__main__':
     main()
-#
-#
-# from tkinter import *
-# from tkinter import ttk
-#
-# root = Tk()
-#
-# content = ttk.Frame(root)
-# frame = ttk.Frame(content, borderwidth=5, relief="sunken", width=200, height=100)
-# namelbl = ttk.Label(content, text="Name")
-# name = ttk.Entry(content)
-#
-# onevar = BooleanVar()
-# twovar = BooleanVar()
-# threevar = BooleanVar()
-# onevar.set(True)
-# twovar.set(False)
-# threevar.set(True)
-#
-# one = ttk.Checkbutton(content, text="One", variable=onevar, onvalue=True)
-# two = ttk.Checkbutton(content, text="Two", variable=twovar, onvalue=True)
-# three = ttk.Checkbutton(content, text="Three", variable=threevar, onvalue=True)
-# ok = ttk.Button(content, text="Okay")
-# cancel = ttk.Button(content, text="Cancel")
-#
-# content.grid(column=0, row=0)
-# frame.grid(column=0, row=0, columnspan=3, rowspan=2)
-# namelbl.grid(column=3, row=0, columnspan=2)
-# name.grid(column=3, row=1, columnspan=2)
-# one.grid(column=0, row=3)
-# two.grid(column=1, row=3)
-# three.grid(column=2, row=3)
-# ok.grid(column=3, row=3)
-# cancel.grid(column=4, row=3)
-#
-# root.mainloop()
